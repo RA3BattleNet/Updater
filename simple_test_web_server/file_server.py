a43123fcab7ffa43391d4a5d5bf25c04eaa3494a
mrbbbaixue <mrbbbaixue@outlook.com>
2026-05-07 23:35:38 +0800

    fix: download 端点改用 os.path.normpath + send_file 修复 Windows 路径问题

--- simple_test_web_server/file_server.py
+++ simple_test_web_server/file_server.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_file, send_from_directory, abort, request, jsonify
+from flask import Flask, send_file, abort, request, jsonify
 import os
 import sys
 import argparse
@@ -34,61 +34,18 @@ def bandwidth_monitor():
 
 threading.Thread(target=bandwidth_monitor, daemon=True).start()
 
-def secure_path(base_dir, filename):
-    base_dir = os.path.abspath(base_dir)
-    requested_path = os.path.abspath(os.path.join(base_dir, filename))
-    if not requested_path.startswith(base_dir):
-        return None
-    return requested_path
-
-@app.route('/')
-def index():
-    return f"""
-    <h1>增量更新本地测试服务器</h1>
-    <ul>
-        <li><a href="/manifest.xml">manifest.xml</a></li>
-        <li><a href="/patches.json">patches.json</a></li>
-        <li><a href="/download/">download 根目录</a></li>
-    </ul>
-    <p>下载格式: <code>/download/files/&lt;md5&gt;</code> 或 <code>/download/patches/&lt;guid&gt;</code></p>
-    <p>服务目录: <code>{SERVER_DIR}</code></p>
-    <hr>
-    <div id="stats">
-        <b>download 路由流量统计：</b><br>
-        入站: {total_in_bytes} B ({format_bytes(total_in_bytes)})<br>
-        出站: {total_out_bytes} B ({format_bytes(total_out_bytes)})<br>
-        入站带宽: {format_bytes(current_in_bandwidth)}/s<br>
-        出站带宽: {format_bytes(current_out_bandwidth)}/s<br>
-    </div>
-    <button onclick="resetStats()">清零</button>
-    <script>
-    function updateStats() {{
-        fetch('/stats')
-            .then(r => r.json())
-            .then(d => {{
-                document.getElementById('stats').innerHTML = `
-                    <b>download 路由流量统计：</b><br>
-                    入站: ${{d.total_in_bytes}} B (${{d.total_in_human}})<br>
-                    出站: ${{d.total_out_bytes}} B (${{d.total_out_human}})<br>
-                    入站带宽: ${{d.in_bandwidth_human}}/s<br>
-                    出站带宽: ${{d.out_bandwidth_human}}/s<br>
-                `;
-            }});
-    }}
-    function resetStats() {{ fetch('/reset_stats', {{method: 'POST'}}); }}
-    setInterval(updateStats, 2000);
-    </script>
-    """
-
 @app.route('/download/<path:filename>')
 def download_file(filename):
     global total_in_bytes, total_out_bytes
-    safe_path = secure_path(SERVER_DIR, filename)
-    if safe_path is None or not os.path.isfile(safe_path):
+    filename = os.path.normpath(filename)
+    path = os.path.join(SERVER_DIR, filename)
+    if not os.path.abspath(path).startswith(os.path.abspath(SERVER_DIR)):
+        abort(404)
+    if not os.path.isfile(path):
         abort(404)
     total_in_bytes += request.content_length or 0
-    total_out_bytes += os.path.getsize(safe_path)
-    return send_from_directory(SERVER_DIR, filename, as_attachment=False)
+    total_out_bytes += os.path.getsize(path)
+    return send_file(path, conditional=True)
 
 @app.route('/manifest.xml')
 def serve_xml():
